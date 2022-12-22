c974b506a850535c69b901341bc51c794ad5c6ba
derek.chen <derek.chen@iyes.dev>
2022-12-22 17:03:39 +0800

    feat : 提升效能

--- src/EpubBook/Services/IThomeService.cs
+++ src/EpubBook/Services/IThomeService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 using System.Web;
 using EpubBook.Interfaces;
 using EpubBook.Models;
@@ -25,6 +26,7 @@ namespace EpubBook.Services
         /// <returns></returns>
         public async Task<EBookDto> CreateEbookDtoFromUrlAsync(string uri)
         {
+            var start = DateTime.Now;
             // 取得網址host
             var host = new Uri(uri).Host;
 
@@ -38,10 +40,10 @@ namespace EpubBook.Services
             document.LoadHtml(response.Content);
 
             // 取得該文章清單
-            var documents = new List<HtmlDocument>();
-            documents.Add(document);
-            var pagings = GetAllPaging(document);
-            documents.AddRange(GetPagingHtmlDocument(pagings));
+            IEnumerable<string> pagings = new List<string>(){
+                uri,
+            };
+            pagings = pagings.Concat(GetAllPaging(document));
 
             // 解析目錄資料
             var creator = "德瑞克";
@@ -51,26 +53,30 @@ namespace EpubBook.Services
             var desc = GetDescription(document);
             var agendas = new List<AgendaDto>();
             var idx = 0;
-            foreach (var doc in documents)
+            await foreach (var doc in GetPagingHtmlDocumentAsync(pagings))
             {
                 agendas.AddRange(GetAgenda(doc, idx).ToList());
                 idx = agendas.Max(o => o.Idx);
             }
 
-            foreach (var agenda in agendas)
+            Console.WriteLine($"time get pagings difference : {DateTime.Now.Subtract(start).TotalSeconds}");
+
+            await foreach (var content in GetContentHtmlDocumentAsync(agendas.Select(o => o.Url)))
             {
-                var content = GetContent(agenda.Url);
+                var agenda = agendas.FirstOrDefault(o => o.Url == content.Item1);
                 agenda.Content = new ContentDto()
                 {
-                    HtmlContent = content,
+                    HtmlContent = content.Item2,
                     ContentName = agenda.Title,
                     Images = new List<ImageDto>()
                 };
-                await foreach (var img in ImageDto.LoadFromHtmlAsync(host, content))
+                await foreach (var img in ImageDto.LoadFromHtmlAsync(host, content.Item2))
                 {
                     agenda.Content.Images.Add(img);
                 }
             }
+
+            Console.WriteLine($"time get Content difference : {DateTime.Now.Subtract(start).TotalSeconds}");
 
             return new EBookDto()
             {
@@ -113,16 +119,57 @@ namespace EpubBook.Services
         /// </summary>
         /// <param name="page_urls"></param>
         /// <returns></returns>
-        private IEnumerable<HtmlDocument> GetPagingHtmlDocument(IEnumerable<string> page_urls)
+        private async IAsyncEnumerable<HtmlDocument> GetPagingHtmlDocumentAsync(IEnumerable<string> page_urls)
         {
+            var tasks = new List<Task<HtmlDocument>>();
             foreach (var url in page_urls)
             {
-                var client = new RestClient();
-                var request = new RestRequest(url);
-                var response = client.Get(request);
-                var doc = new HtmlDocument();
-                doc.LoadHtml(response.Content);
-                yield return doc;
+                Console.WriteLine($"GetPagingHtmlDocumentAsync : {url}");
+                tasks.Add(GetHtmlDocumentAsync(url));
+            }
+            var taskWhenAll =await Task.WhenAll(tasks.ToArray());
+            foreach (var result in taskWhenAll)
+            {
+                yield return result;
+            }
+        }
+
+        private async Task<HtmlDocument> GetHtmlDocumentAsync(string url)
+        {
+            var client = new RestClient();
+            var request = new RestRequest(url);
+            var response = await client.GetAsync(request);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response.Content);
+            return doc;
+        }
+
+        private async Task<(string,HtmlDocument)> GetHtmlDocumentWithUrlAsync(string url)
+        {
+            var client = new RestClient();
+            var request = new RestRequest(url);
+            var response = await client.GetAsync(request);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(response.Content);
+            return (url,doc);
+        }
+
+        public async IAsyncEnumerable<(string, string)> GetContentHtmlDocumentAsync(IEnumerable<string> urls)
+        {
+            var tasks = new List<Task<(string, HtmlDocument)>>();
+            foreach (var url in urls)
+            {
+                Console.WriteLine($"GetContentHtmlDocumentAsync : {url}");
+                tasks.Add(GetHtmlDocumentWithUrlAsync(url));
+            }
+
+            var taskWhenAll = await Task.WhenAll(tasks.ToArray());
+            foreach (var result in taskWhenAll)
+            {
+                var doc = result.Item2;
+                var content_Node = doc.DocumentNode.SelectSingleNode("/html/body/div[2]/div/div/div[1]/div[2]/div[3]/div[2]");
+                var content = content_Node.InnerHtml.Trim();
+                yield return (result.Item1, content);
             }
         }
 
@@ -179,18 +226,6 @@ namespace EpubBook.Services
             var title_desc_Node = document.DocumentNode.SelectSingleNode("/html/body/div[2]/div/div/div[2]/div[1]/div[2]/p");
             var title_desc = title_desc_Node.InnerText.Trim();
             return title_desc;
-        }
-
-        public string GetContent(string url)
-        {
-            var client = new RestClient();
-            var request = new RestRequest(url);
-            var response = client.Get(request);
-            var doc = new HtmlDocument();
-            doc.LoadHtml(response.Content);
-            var content_Node = doc.DocumentNode.SelectSingleNode("/html/body/div[2]/div/div/div[1]/div[2]/div[3]/div[2]");
-            var content = content_Node.InnerHtml.Trim();
-            return content;
         }
     }
 }
